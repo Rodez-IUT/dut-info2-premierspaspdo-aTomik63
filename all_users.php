e28c14d9572b7d8e04da491430ec5cfcd91a04cb
alexandre.biet <alexandre.biet@iut-rodez.fr>
2019-11-13 10:20:25 +0100

    fix #2 Récupération et affichage des utilisateurs avec énoncé non préparé

--- all_users.php
+++ all_users.php
@@ -16,4 +16,23 @@
 	} catch (PDOException $e) {
 		 throw new PDOException($e->getMessage(), (int)$e->getCode());
 	}
+	
+	$stmt = $pdo->query('select users.id as user_id, username, email, s.name as status from users join status s on users.status_id = s.id');
 ?>
+
+<table>
+    <tr>
+        <th>Id</th>
+        <th>Username</th>
+        <th>Email</th>
+        <th>Status</th>
+    </tr>
+    <?php while ($row = $stmt->fetch()) { ?>
+    <tr>
+        <td><?php echo $row['user_id']?></td>
+        <td><?php echo $row['username']?></td>
+        <td><?php echo $row['email']?></td>
+        <td><?php echo $row['status']?></td>
+    </tr>
+    <?php } ?>
+</table>
